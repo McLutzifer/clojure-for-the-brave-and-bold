c24398b796afef8a7f5189eb0ffaf703ad970e49
McLutzifer <lukasschumi@gmx.at>
2021-08-27 16:30:32 +0200

    namespace

--- LearnXinYminutes/whereXisClojure.clj
+++ LearnXinYminutes/whereXisClojure.clj
@@ -1,2 +1,11 @@
 ; Comments start with semicolons.
 
+; The clojure reader assumes that the first thing is a
+; function or macro to call, and the rest are arguments.
+
+; The first call in a file should be ns, to set the namespace
+
+(ns learnclojure)
+
+; more basic examples:
+
